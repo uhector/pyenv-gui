7b56549ec047325b39514d38770777336e62148d
Hector Ulacio <hectorulacior@gmail.com>
2020-05-04 00:13:54 -0400

    Create `Treeview` class

--- pyenv_manager/gui/components/treeview.py
+++ pyenv_manager/gui/components/treeview.py
@@ -2,3 +2,10 @@
 
 import tkinter as tk
 from tkinter import ttk
+
+class Treeview(ttk.Treeview):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.heading('#0', text='Installed versions')
