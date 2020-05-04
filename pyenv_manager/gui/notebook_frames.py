95ac845df74fec9f1fb6c7dcec5f85ace0350a3b
Hector Ulacio <hectorulacior@gmail.com>
2020-05-04 00:22:37 -0400

    Create `VersionManagementFrame` class

--- pyenv_manager/gui/notebook_frames.py
+++ pyenv_manager/gui/notebook_frames.py
@@ -2,3 +2,18 @@
 
 import tkinter as tk
 from tkinter import ttk
+
+from .components.treeview import Treeview
+
+class VersionManagementFrame(ttk.Frame):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.treeview = Treeview(self)
+        self.treeview.grid(column=0, row=0, columnspan=2)
+
+        ttk.Button(self, text='Set as global').grid(column=0, row=1)
+        ttk.Button(self, text='Install version').grid(column=1, row=1)
+
+        self.pack()
