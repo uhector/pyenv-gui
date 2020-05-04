3580b5458a975d2b1b5316c01c5404d5ed6bf8ff
Hector Ulacio <hectorulacior@gmail.com>
2020-05-04 00:33:26 -0400

    Add `Notebook` widget

--- pyenv_manager/gui/main_frame.py
+++ pyenv_manager/gui/main_frame.py
@@ -3,10 +3,14 @@
 import tkinter as tk
 from tkinter import ttk
 
+from .notebook import Notebook
+
 class MainFrame(ttk.Frame):
 
     def __init__(self, parent):
         super().__init__(parent)
+
+        self.notebook = Notebook(self)
 
         parent.title("Pyenv Manager")
         self.pack()
