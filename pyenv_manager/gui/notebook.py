820cf69d5e591094892d87de0fca555c408e45d5
Hector Ulacio <hectorulacior@gmail.com>
2020-05-04 00:23:35 -0400

    Create `Notebook` class

--- pyenv_manager/gui/notebook.py
+++ pyenv_manager/gui/notebook.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import tkinter as tk
-from tkinter import ttk        
+from tkinter import ttk
+
+from .notebook_frames import VersionManagementFrame
+
+class Notebook(ttk.Notebook):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        # Version Management tab
+        self.add(VersionManagementFrame(self),
+                 text='Version Management',
+                 padding=5
+        )
+
+        self.pack()
