fcf0710c35b92ccc46ae4f14fc17942f0aded36f
Hector Ulacio <hectorulacior@gmail.com>
2020-05-08 20:37:48 -0400

    Add `_set_as_global` method

--- pyenv_manager/gui/notebook_frames.py
+++ pyenv_manager/gui/notebook_frames.py
@@ -5,6 +5,8 @@ from tkinter import ttk
 
 from .components.treeview import Treeview
 
+from . import pyenv_interface
+
 class VersionManagementFrame(ttk.Frame):
 
     def __init__(self, *args, **kwargs):
@@ -12,8 +14,24 @@ class VersionManagementFrame(ttk.Frame):
 
         self.treeview = Treeview(self)
         self.treeview.grid(column=0, row=0, columnspan=2)
+        self.treeview.update()
 
-        ttk.Button(self, text='Set as global').grid(column=0, row=1)
+        ttk.Button(self, text='Set as global',
+                   command=self._set_as_global).grid(column=0, row=1)
+
         ttk.Button(self, text='Install version').grid(column=1, row=1)
 
         self.pack()
+
+
+    def _set_as_global(self):
+        tree = self.treeview
+        selected_version = tree.item(tree.selection())['text']
+        if pyenv_interface.get_global_version() == selected_version[:-1]:
+            pass
+        else:
+            if selected_version == 'system':
+                pyenv_interface.set_global_version('')
+            else:
+                pyenv_interface.set_global_version(selected_version)
+        tree.update()
