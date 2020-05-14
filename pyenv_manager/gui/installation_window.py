8bf31b9b916eb0a839da9f7ccf3f12c0542e31d5
Hector Ulacio <hectorulacior@gmail.com>
2020-05-13 22:06:14 -0400

    Create 'installation_window.py'

---
+++ pyenv_manager/gui/installation_window.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import tkinter as tk
+
+class InstallationWindow(tk.Toplevel):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title('Install version')
