25265d2d5b91f65d366fbc788da4600abb1536ca
Hector Ulacio <hectorulacior@gmail.com>
2020-05-14 23:19:07 -0400

    Add `InstallationFrame` object

--- pyenv_manager/gui/installation_window.py
+++ pyenv_manager/gui/installation_window.py
@@ -2,9 +2,12 @@
 
 import tkinter as tk
 
+from .frames.installation_frame import InstallationFrame
+
 class InstallationWindow(tk.Toplevel):
 
     def __init__(self):
         super().__init__()
 
         self.title('Install version')
+        self.frame = InstallationFrame(self)
