0f86f1af8ff84d42e55f5f0c04d5c2b1e56b807e
Hector Ulacio <hectorulacior@gmail.com>
2020-05-17 23:14:00 -0400

    Add `parse_output` function

--- pyenv_manager/interface/helpers.py
+++ pyenv_manager/interface/helpers.py
@@ -6,3 +6,13 @@ def parse_ls_output(output):
     for string in strings:
         directories.append(string.split(' ')[-1])
     return directories[:-1]
+
+def parse_output(output):
+    result = []
+    strings = output.split('\n')
+    del strings[0]
+    del strings[-1]
+    for string in strings:
+        if string[2].isnumeric():
+            result.append(string.strip())
+    return result
