a749bb897c87f9f400cf3a519549ecb16983992a
Hector Ulacio <hectorulacior@gmail.com>
2020-05-22 22:18:40 -0400

    Add `insert_text` method

--- pyenv_manager/gui/components/text.py
+++ pyenv_manager/gui/components/text.py
@@ -6,3 +6,7 @@ class Text(tk.Text):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+    def insert_text(self, text):
+        self.insert(tk.INSERT, text)
