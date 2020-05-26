b70299845d1efba9eeb76409da230bbcd5efb1eb
Hector Ulacio <hectorulacior@gmail.com>
2020-05-25 20:13:32 -0400

    Add `insert_from_file` method

--- pyenv_manager/gui/components/text.py
+++ pyenv_manager/gui/components/text.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import tkinter as tk
+import time
 
 class Text(tk.Text):
 
@@ -9,7 +10,17 @@ class Text(tk.Text):
 
 
     def insert_text(self, text):
-        self.insert(tk.INSERT, text)
+        self.insert(tk.END, text)
+
+    def insert_from_file(self, interface):
+        while interface.installing_version:
+            with open('logs.txt', 'r') as file:
+                self.config(state='normal')
+                self.clean()
+                self.insert_text(file.read())
+                self.scroll_down()
+                self.config(state='disable')
+            time.sleep(1)
 
     def clean(self):
         self.delete(1.0, tk.END)
